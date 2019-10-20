02e87e05be746c2b8bb978464ba8c8a9fd875475
Michael Jewell <michaeljewell9911@gmail.com>
2019-10-20 09:58:01 -0700

    mj  - get of inheritance working

--- src/FormObject.ts
+++ src/FormObject.ts
@@ -1,26 +1,57 @@
 import { FieldType } from "./interfaces";
 import { map, forEach } from "./collections/object";
 
-export interface ShapeType {
-  [key: string]: { new (value: unknown): FieldType };
+export interface ShapeType<F extends FieldType = FieldType> {
+  [key: string]: { new (value: unknown): F };
 }
 
-export type FormObjectFields<Shape extends ShapeType> = {
-  [K in keyof Shape]?: InstanceType<Shape[K]>;
+export type FormObjectFields<
+  Shape extends ShapeType<F>,
+  F extends FieldType = FieldType
+> = {
+  [K in keyof Shape]: InstanceType<Shape[K]>;
 };
 
-export type FormObjectValues<Shape extends ShapeType> = {
-  [K in keyof Shape]?: InstanceType<Shape[K]>["value"];
+export type FormObjectValues<
+  Shape extends ShapeType<F>,
+  F extends FieldType = FieldType
+> = {
+  [K in keyof Shape]: InstanceType<Shape[K]>["value"];
 };
 
-export class FormObject<Shape extends ShapeType> {
+export class FormObject<
+  Shape extends ShapeType<F>,
+  F extends FieldType = FieldType
+> implements FieldType {
+  public static of<
+    T extends typeof FormObject,
+    Shape extends ShapeType<F>,
+    F extends FieldType = FieldType
+  >(
+    this: T,
+    shape: Shape
+  ): {
+    new (value: FormObjectValues<Shape, F>): FormObject<Shape> &
+      InstanceType<T>;
+  } {
+    const SuperClass = this as new (...args: any[]) => FormObject<Shape, F>;
+    return class DefinedFormObject extends SuperClass {
+      public constructor(value: FormObjectValues<Shape, F>) {
+        super(shape, value);
+      }
+    } as {
+      new (value: FormObjectValues<Shape, F>): FormObject<Shape> &
+        InstanceType<T>;
+    };
+  }
+
   private shape: Shape;
 
-  public fields!: FormObjectFields<Shape>;
+  public fields: FormObjectFields<Shape>;
 
   public constructor(shape: Shape, value: FormObjectValues<Shape>) {
     this.shape = shape;
-    this.fields = {};
+    this.fields = ({} as any) as FormObjectFields<Shape>;
     this.setValue(value);
   }
 
--- src/__tests__/GoodForm.test.ts
+++ src/__tests__/GoodForm.test.ts
@@ -1,11 +1,38 @@
-import { GoodForm } from "../GoodForm";
+import { Field } from "../Field";
+import { FormObject, FormObjectValues, ShapeType } from "../FormObject";
+
+class StringField extends Field<string> {}
+class NumberField extends Field<number> {}
+
+it("does stuff", (): void => {
+  class X<T extends ShapeType> extends FormObject<T> {
+    public get x(): FormObjectValues<T> {
+      return this.value;
+    }
+  }
+
+  class Y<T extends ShapeType> extends X<T> {
+    public get y(): FormObjectValues<T> {
+      return this.value;
+    }
+  }
+
+  const F = Y.of({
+    first: StringField
+  });
+
+  const f = new F({
+    first: "1"
+  });
+
+  console.log(f);
+  console.log(f.x);
+  console.log(f.y);
+});
 
 describe("complex examples", (): void => {
-  class StringField extends GoodForm.Field<string> {}
-  class NumberField extends GoodForm.Field<number> {}
-
   it("handles objects", (): void => {
-    const Form = GoodForm.Object({
+    const Form = FormObject.of({
       name: StringField,
       age: NumberField
     });
@@ -32,17 +59,24 @@ describe("complex examples", (): void => {
   });
 
   it("handles nested objects", (): void => {
-    const Form = GoodForm.Object({
-      user: GoodForm.Object({
-        name: StringField,
-        age: NumberField
-      })
+    const UserForm = FormObject.of({
+      name: StringField,
+      age: NumberField
+    });
+
+    const userForm = new UserForm({
+      name: "",
+      age: "1"
+    });
+
+    const Form = FormObject.of({
+      user: UserForm
     });
 
     const form = new Form({
       user: {
         name: "a",
-        age: 1
+        age: "1" // this should fail
       }
     });
 
